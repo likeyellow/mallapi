533990e9e57d89be0905151a08fff095c61439f3
likeyellow <likeyellowand@gmail.com>
2024-06-28 15:03:02 +0900

    에러 메시지 if~else문 리팩토링

--- src/main/java/org/zerock/mallapi/security/handler/APILoginFailHandler.java
+++ src/main/java/org/zerock/mallapi/security/handler/APILoginFailHandler.java
@@ -46,22 +46,24 @@ public class APILoginFailHandler implements AuthenticationFailureHandler {
 
         if(exception instanceof BadCredentialsException || exception instanceof InternalAuthenticationServiceException) {
             errorMessage = "Username과 Password가 맞지 않습니다. 다시 확인해 주십시오.";
-            //errorCode = 500;
-        } else if(exception instanceof DisabledException) {
+        } 
+
+        if(exception instanceof DisabledException) {
             errorMessage = "계정이 비활성화 되었습니다. 관리자에게 문의하세요.";
-            //errorCode = 501;
-        } else if(exception instanceof InternalAuthenticationServiceException) {
+        } 
+        
+        if(exception instanceof InternalAuthenticationServiceException) {
             errorMessage = "내부적으로 발생한 시스템 문제로 인해 요청을 처리할 수 없습니다. 관리자에게 문의하세요.";
-            //errorCode = 502;
-        } else if(exception instanceof UsernameNotFoundException) {
+        } 
+        
+        if(exception instanceof UsernameNotFoundException) {
             errorMessage = "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요.";
-            //errorCode = 503;
-        } else if(exception instanceof AuthenticationCredentialsNotFoundException) {
+        } 
+        
+        if(exception instanceof AuthenticationCredentialsNotFoundException) {
             errorMessage = "인증 요청이 거부되었습니다. 관리자에게 문의하세요.";
-            //errorCode = 504;
         } else {
             errorMessage = "알 수 없는 이유로 로그인에 실패하였습니다. 관리자에게 문의하세요.";
-            //errorCode = 404;
         }
 
         Gson gson = new Gson();
